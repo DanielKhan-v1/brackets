c07bf188621fa88e2d261b53cb8ca0109c276a95
danielkhan-v1 <grigorovich314@gmail.com>
2021-03-10 22:11:33 +0200

    feat: create solution for brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let temp = "";
+
+    for (let x = bracketsConfig.length + 1; x > 0; x--) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            for (let item of bracketsConfig[i]) {
+                temp += item;
+            }
+
+            while (str.includes(temp)) {
+                str = str.replace(temp, "");
+            }
+
+            temp = "";
+        }
+    }
+
+    if (str.length === 0) return true;
+    else return false;
+};
